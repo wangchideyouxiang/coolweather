4a36b67253c2da1648266f7bf2acfabfe0497071
wangchi <122252357@qq.com>
2016-11-04 21:43:42 +0800

    新增数据库帮助类，以及各表对应的实体类.

---
+++ app/src/main/java/com/app/coolweather/model/City.java
@@ -0,0 +1,36 @@
+package com.app.coolweather.model;
+
+/**
+ * Created by 王驰 on 2016-11-04.
+ */
+
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCityName() {
+        return cityName;
+    }
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+    public String getCityCode() {
+        return cityCode;
+    }
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+    public int getProvinceId() {
+        return provinceId;
+    }
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
